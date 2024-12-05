403d112fc7664aff371717baa95aef72b2ab3a76
jordiguitartr <jguitartr@gmail.com>
2024-12-05 12:47:42 +0100

    icon

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,6 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    protected $table="user";
 
     public function comandes()
     {
