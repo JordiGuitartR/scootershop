f8af44a2bcb03554c671193c21dfd12e482dff32
jordiguitartr <jguitartr@gmail.com>
2024-12-10 10:33:28 +0100

    homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,27 @@
 <x-guest-layout>
-    <div class="py-5">
-        <div class="container max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{__('Home page')}}
-            </h2>
-            <section>
-                <p>
-                    Lorem ipsum dolor,sit amet awildalkahba genis puteru
-                </p>
-            </section>
-             
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Homepage') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-gray-300 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("Here you will find some information about this website, a short introduction in order to not get lost!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="py">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-gray-300 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("Here you will find some information about this website, a short introduction in order to not get lost!") }}
+                </div>
+            </div>
         </div>
     </div>
 
